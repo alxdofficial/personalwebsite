2f9e51839f815d687e3dfa30236f9ca58052ab8c
Alex Ding <maleficent219@gmail.com>
2022-08-07 21:58:25 -0700

    .

---
+++ basic_routes.py
@@ -0,0 +1,10 @@
+from app import db
+from models import User
+from flask import request, render_template, flash, redirect
+from app import app
+
+@app.route('/',methods = ['get'])
+def hello_world():
+    return render_template()
+
+
---
+++ models.py
@@ -0,0 +1,16 @@
+from app import db
+from flask_login import UserMixin
+from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
+
+class User(db.Model, UserMixin):
+    __table_args__ = {'extend_existing': True}
+    __table_name__ = 'User'
+    id = db.Column(db.VARCHAR(10), primary_key=True, unique=True,index=True)
+    name = db.Column(db.String(50),index=True)
+    email = db.Column(db.String(120), unique=True, index=True, nullable=False)
+    password_hash = db.Column(db.VARCHAR(128))
+    type = db.Column(db.VARCHAR(20),index=True)
+
+    def __repr__(self):
+        return '<User {}>'.format(self.name)
