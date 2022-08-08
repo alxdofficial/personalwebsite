be58fe7fe49fb24f882680bc9da23156f0f8dde1
Alex Ding <maleficent219@gmail.com>
2022-08-08 00:22:07 -0700

    .

--- app.py
+++ app.py
@@ -13,4 +13,4 @@ migrate = Migrate(app, db)
 from basic_routes import *
 
 if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0')
+    app.run(host='0.0.0.0')
--- basic_routes.py
+++ basic_routes.py
@@ -3,7 +3,7 @@ from models import User
 from flask import request, render_template, flash, redirect
 from app import app
 
-@app.route('/',methods = ['GET','POST'])
+@app.route('/',methods = ['GET'])
 def index():
     return render_template('index.html')
 
